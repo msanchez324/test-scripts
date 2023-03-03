3d481ab41dde02d83217379e5fa5c2295e45efef
msanchez324 <melvinsanchezsosa@gmail.com>
2023-03-03 15:01:03 -0400

    Update workerAI.cs

--- Scripts/workerAI.cs
+++ Scripts/workerAI.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class worker : MonoBehaviour
+public class workerAI : MonoBehaviour
 {
     NavMeshAgent agent;
     public Transform[] waypoints;
