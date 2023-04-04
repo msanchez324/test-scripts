afab0749d26697af0382efd27e3da50f73b3d09a
msanchez324 <melvinsanchezsosa@gmail.com>
2023-04-04 14:23:16 -0400

    initial commit

--- Scripts/NPCPatrol.cs
+++ Scripts/NPCPatrol.cs
@@ -1,40 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class NPCPatrol : MonoBehaviour
 {
-    public float movSpeed;
-    public Transform[] waypoints;
-    private int currentWaypointIndex = 0;
-    //private float minDistance;
-    //private int randomNumber;
-    //private Renderer renderer;
+    private CharacterController characterController;
 
+    public float movSpeed;  //NPC movement Speed
+    public Transform[] waypoints;   //
+    private int currentWaypointIndex = 0;   //
+    public bool temp = false;
     // Start is called before the first frame update
     void Start()
     {
-      
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+            
+        Debug.Log(temp);
+         temp = pos(temp);
+    }
+
+    bool pos(bool temp)
+    {
         // el punto actual en el qu se encuentra
-        Transform waypoint = waypoints[currentWaypointIndex];
-        if (Vector3.Distance(transform.position, waypoint.position) < 0.01f)
-        {
-            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
-        }
-        else
+        if (transform.position == waypoints[currentWaypointIndex].position)
+            increaseCurrentWaypointIndex();
+        if (temp == true)
         {
             transform.position = Vector3.MoveTowards(
                 transform.position,
-                waypoint.position,
+                waypoints[currentWaypointIndex].position,
                 movSpeed * Time.deltaTime);
             //Hacia donde mirara el NPC
-            transform.LookAt(waypoint.position);
+            transform.LookAt(waypoints[currentWaypointIndex].position);
         }
+
+        if (transform.position == waypoints[currentWaypointIndex].position)
+            temp = false;
+        return temp;
+    }
+
+    void increaseCurrentWaypointIndex()
+    {
+        currentWaypointIndex++;
+        if(currentWaypointIndex >= waypoints.Length)
+            currentWaypointIndex = 0;
     }
 }
